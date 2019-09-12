8d60e385f7c954366c9907246526bed723e99602
Bryce Bjork <brycedbjork@gmail.com>
2019-09-12 16:02:42 -0400

    change subject of email:

--- index.py
+++ index.py
@@ -21,5 +21,5 @@ ip = get_ip()
 
 r = requests.post("https://api.mailgun.net/v3/mg.sandboxatyale.com/messages", auth=("api", "255f445b4e76bfadfba7bf961a98592d-4a62b8e8-9a66ec05"), data={"from": "Bryce's Pi <bryces-pi@sandboxatyale.com>",
                                                                                                                                                         "to": ["bryce.bjork@yale.edu"],
-                                                                                                                                                        "subject": "Pi IP",
+                                                                                                                                                        "subject": "IP",
                                                                                                                                                         "text": ip})
