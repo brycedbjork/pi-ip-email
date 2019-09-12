8aa11901489423e96b8d8846f34bf650201a9a4d
Bryce Bjork <brycedbjork@gmail.com>
2019-09-12 15:36:53 -0400

    working ip

--- index.py
+++ index.py
@@ -4,16 +4,20 @@ import fcntl
 import struct
 
 
-def get_ip_address(ifname):
+def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 
-ip = get_ip_address('eth0')
+ip = get_ip()
 
 r = requests.post("https://api.mailgun.net/v3/mg.sandboxatyale.com/messages", auth=("api", "255f445b4e76bfadfba7bf961a98592d-4a62b8e8-9a66ec05"), data={"from": "Bryce's Pi <bryces-pi@sandboxatyale.com>",
                                                                                                                                                         "to": ["bryce.bjork@yale.edu"],
