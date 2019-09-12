6367a51f95b97a8a938d36598aa80ecbba4a312a
Bryce Bjork <brycedbjork@gmail.com>
2019-09-12 15:29:25 -0400

    network ip

--- index.py
+++ index.py
@@ -1,6 +1,19 @@
-import socket
 import requests
-ip = socket.gethostbyname(socket.gethostname())
+import socket
+import fcntl
+import struct
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+
+ip = get_ip_address('eth0')
 
 r = requests.post("https://api.mailgun.net/v3/mg.sandboxatyale.com/messages", auth=("api", "255f445b4e76bfadfba7bf961a98592d-4a62b8e8-9a66ec05"), data={"from": "Bryce's Pi <bryces-pi@sandboxatyale.com>",
                                                                                                                                                         "to": ["bryce.bjork@yale.edu"],
